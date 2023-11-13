a41631c950dab9f136b09b902389167339f2ef9e
Carlos Salguero <carlossalguero441@gmail.com>
2023-11-13 10:49:48 -0600

    feat(Docker): Docker implementation Created a docker container for managing dependencies with ease

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
-#!/bin/zsh
+#!/bin/bash
 
-# Create build directory if it doesn't exist
-mkdir -p build
-cd build
+# Pull the Docker image (if not already pulled)
+docker pull crsa441/regex-to-dfa
 
-# Run cmake
-cmake ..
-make
+# Define the source and build directories on the host machine
+SOURCE_DIR="$(pwd)"  # Assuming CMakeLists.txt is in the current directory
+BUILD_DIR="$(pwd)/build"
 
-# Run the program
-./bin/RegexToDFAConverter
+# Run the program inside the Docker container using /bin/bash
+docker run -it --rm -v "$SOURCE_DIR:/mnt/source" -v "$BUILD_DIR:/mnt/build" crsa441/regex-to-dfa /bin/bash -c "cd /mnt/build && cmake /mnt/source && make && ./bin/RegexToDFAConverter"
