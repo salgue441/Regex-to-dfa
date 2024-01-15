1dbe734e2e123675cdd6c273576ba197b746a37b
Carlos Salguero <carlossalguero441@gmail.com>
2024-01-15 14:54:51 -0600

    feat(Lexer): Lexer reestructure Implemented a new lexer class with exception handling and boost library usage. This new namespace considers a new definition of token with the position to be saved at, as well as, logging information and errors using spdlog

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
-int main()
+#include "lexer/lexer.h"
+
+int main(int argc, char const **argv)
 {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+  try
+  {
+    std::string input = "a|b";
+    lexer::Lexer lexer(input);
+
+    std::vector<lexer::Token> tokens = lexer.analyze();
+
+    for (const auto &token : tokens)
+      std::cout << "Type: " << static_cast<int>(token.m_type)
+                << " Value: '" << token.m_value << "'\n";
+  }
+  catch (const std::exception &e)
+  {
+    std::cerr << "Exception: " << e.what() << std::endl;
+  }
 }
